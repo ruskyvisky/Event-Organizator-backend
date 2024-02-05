95fb375c4be4c38998461e4e607960c705a7109a
ruskyvisky <numan_859@hotmail.com>
2024-02-05 10:59:26 +0300

    Initial commit

---
+++ src/main/java/com/eventorganizator/Event/Organizator/EventOrganizatorApplication.java
@@ -0,0 +1,13 @@
+package com.eventorganizator.Event.Organizator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventOrganizatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventOrganizatorApplication.class, args);
+	}
+
+}
