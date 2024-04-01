65df1249afd65c472fb121846192d6fadb2a849f
ruskyvisky <numan_859@hotmail.com>
2024-04-01 06:55:50 +0300

    fixed cors problem

--- src/main/java/com/eventorganizator/Event/Organizator/EventOrganizatorApplication.java
+++ src/main/java/com/eventorganizator/Event/Organizator/EventOrganizatorApplication.java
@@ -3,12 +3,25 @@ package com.eventorganizator.Event.Organizator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class EventOrganizatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventOrganizatorApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
